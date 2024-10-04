821c4086e8c473a9b69c20e2c986c81f80a56c83
zxq <xtvjxk123456maya@163.com>
2024-10-04 12:23:49 +0800

    changed

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import  json
 sentences = ['苹果']
 model = SentenceModel(r"D:\Code\text2vec-base-chinese")
 embeddings = model.encode(sentences)
-print(json.dumps(embeddings.tolist()[0]))
+# print(json.dumps(embeddings.tolist()[0]))
 print('dimension size of vector is: {}'.format(len(json.dumps(embeddings.tolist()[0]))))
 
 # model.encode("如何更换花呗绑定银行卡")
